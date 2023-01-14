1f51e61356d7d26901a3c188ea539b8c544ae3c2
Eetu Haanpää <eehaan@utu.fi>
2023-01-14 23:23:40 +0200

    first commit

---
+++ src/bot.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const { discord, Client, Intents, GatewayIntentBits } = require('discord.js');
+
+//Botin alustaminen ja oikeuksien kanssa säätämistä.
+const client = new Client({ intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+    allowedMentions: {parse: ['users', 'roles'], repliedUser: true}
+});
+
+client.on('ready', () => {
+    console.log(`sisäänkirjautuminen käyttäjänä: ${client.user.tag}`)
+});
+
+client.on('messageCreate', (message) => {
+    //Ignore messages sent by the bot.
+    if (message.author.bot) return;
+    console.log(`[${message.author.tag}]: ${message.content}`)
+
+    //Kirjoittaa chattiin botin viiveen komennolla ".ping"
+    if (message.content === '.ping'){
+        message.channel.send(`Pong! viiveet: ${Date.now() - message.createdTimestamp}ms, ${client.ws.ping}ms `);
+    }
+
+    
+});
+
+client.login(process.env.DISCORDJS_BOT_TOKEN);
